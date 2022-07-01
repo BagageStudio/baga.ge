016e4240296f34fe0d629fc12843039449e48ca5
menthe <adrien.le.mentheour@gmail.com>
2022-07-01 17:35:57 +0200

    🔧 build output directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    "nitro": {
+        "output": {
+            dir: 'output',
+            serverDir: 'output/server',
+            publicDir: 'output/public'
+        }
+    },
     typescript: {
         shim: false
     },
