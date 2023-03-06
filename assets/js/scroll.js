638fb09a70ecba419d091bb6651dc55070ea5fc0
Clément Lemoine <clementlemoine.cl@gmail.com>
2023-03-06 17:17:58 +0100

    🐛 remove project sticky on mobile

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -17,103 +17,107 @@ export function CreateScroll() {
 }
 
 export function CreateProjectsAnimation() {
-    /** --- SMOOTH STICKY --- **/
-    /**
-     * VVO = Vertical Offset we want Visually
-     *     = The offset that was initially in the CSS "top" on the "position: sticky" element ) : 150px
-     *     = 150px
-     *
-     * ED  = Easing Distance
-     *     = The distance the element should translate up to smooth the sticky. The larger the smoother
-     *     = 200px
-     *
-     * PPT = Parent Padding Top (optionnal)
-     *     = In our case we are watching the parent (#projectsMonolith) entering and leaving the screen.
-     *       If it has a padding top we should keep it in mind for our calculations
-     *     = 130px
-     *
-     * PPB = Parent Padding Bottom (optionnal)
-     *     = In our case we are watching the parent (#projectsMonolith) entering and leaving the screen.
-     *       If it has a padding bottom we should keep it in mind for our calculations
-     *     = 200px
-     *
-     * H   = Sticky Element Height
-     *     = Computed from javascript, we're using it like this ${stickyElement.offsetHeight} to have it dynamic (auto recalculate on resize)
-     *
-     * With these 3 values we can set the parameters:
-     *
-     * CSS "top" on the sticky element
-     *     = VVO + ED
-     *
-     * CSS "padding-bottom" on the sticky element
-     *     = ED
-     *
-     * scrollTrigger In "start" param
-     *     = VVO + ED - PPT
-     *
-     * scrollTrigger In "end" param
-     *     = -ED
-     *
-     * scrollTrigger In "y" transform
-     *     = -ED
-     *
-     * scrollTrigger Out "start" param. Use a function to have the H dynamic (auto recalculate on resize)
-     *     = VVO + ED + PPB + H
-     *
-     * scrollTrigger Out "end" param
-     *     = PPB
-     *
-     * scrollTrigger Out "y" transform
-     *     = ED
-     *
-     */
+    const mm = gsap.matchMedia();
 
-    const stickyElement =
-        document.getElementById("projectsTitle").parentElement;
+    mm.add("(min-width: 1100px)", () => {
+        /** --- SMOOTH STICKY --- **/
+        /**
+         * VVO = Vertical Offset we want Visually
+         *     = The offset that was initially in the CSS "top" on the "position: sticky" element ) : 150px
+         *     = 150px
+         *
+         * ED  = Easing Distance
+         *     = The distance the element should translate up to smooth the sticky. The larger the smoother
+         *     = 200px
+         *
+         * PPT = Parent Padding Top (optionnal)
+         *     = In our case we are watching the parent (#projectsMonolith) entering and leaving the screen.
+         *       If it has a padding top we should keep it in mind for our calculations
+         *     = 130px
+         *
+         * PPB = Parent Padding Bottom (optionnal)
+         *     = In our case we are watching the parent (#projectsMonolith) entering and leaving the screen.
+         *       If it has a padding bottom we should keep it in mind for our calculations
+         *     = 200px
+         *
+         * H   = Sticky Element Height
+         *     = Computed from javascript, we're using it like this ${stickyElement.offsetHeight} to have it dynamic (auto recalculate on resize)
+         *
+         * With these 3 values we can set the parameters:
+         *
+         * CSS "top" on the sticky element
+         *     = VVO + ED
+         *
+         * CSS "padding-bottom" on the sticky element
+         *     = ED
+         *
+         * scrollTrigger In "start" param
+         *     = VVO + ED - PPT
+         *
+         * scrollTrigger In "end" param
+         *     = -ED
+         *
+         * scrollTrigger In "y" transform
+         *     = -ED
+         *
+         * scrollTrigger Out "start" param. Use a function to have the H dynamic (auto recalculate on resize)
+         *     = VVO + ED + PPB + H
+         *
+         * scrollTrigger Out "end" param
+         *     = PPB
+         *
+         * scrollTrigger Out "y" transform
+         *     = ED
+         *
+         */
 
-    /** --- SMOOTH STICKY IN --- **/
-    gsap.to("#projectsTitle", {
-        scrollTrigger: {
-            trigger: "#projectsMonolith",
-            scrub: true,
-            start: "top 220px",
-            end: "top -200px",
-        },
-        y: -200,
-        ease: "sine.out",
-    });
+        const stickyElement =
+            document.getElementById("projectsTitle").parentElement;
 
-    /** --- SMOOTH STICKY OUT --- **/
-    gsap.fromTo(
-        "#projectsTitle",
-        {
-            y: -200,
-        },
-        {
+        /** --- SMOOTH STICKY IN --- **/
+        gsap.to("#projectsTitle", {
             scrollTrigger: {
                 trigger: "#projectsMonolith",
                 scrub: true,
-                start: () => `bottom ${550 + stickyElement.offsetHeight}px`,
-                end: "bottom 200px",
+                start: "top 220px",
+                end: "top -200px",
             },
-            y: 200,
+            y: -200,
             ease: "sine.out",
-            immediateRender: false,
-        }
-    );
+        });
 
-    /** --- DRAWING LINES --- **/
-    const lines = document.querySelectorAll(".overflow-line");
-    [...lines].forEach((line) => {
-        gsap.to(line, {
-            scrollTrigger: {
-                trigger: line,
-                scrub: true,
-                start: "top 80%",
-                end: "top 30%",
+        /** --- SMOOTH STICKY OUT --- **/
+        gsap.fromTo(
+            "#projectsTitle",
+            {
+                y: -200,
             },
-            scaleX: 0,
-            ease: "power2.inOut",
+            {
+                scrollTrigger: {
+                    trigger: "#projectsMonolith",
+                    scrub: true,
+                    start: () => `bottom ${550 + stickyElement.offsetHeight}px`,
+                    end: "bottom 200px",
+                },
+                y: 200,
+                ease: "sine.out",
+                immediateRender: false,
+            }
+        );
+
+        /** --- DRAWING LINES --- **/
+        const lines = document.querySelectorAll(".overflow-line");
+        [...lines].forEach((line) => {
+            gsap.to(line, {
+                scrollTrigger: {
+                    trigger: line,
+                    scrub: true,
+                    start: "top 80%",
+                    end: "top 30%",
+                },
+                scaleX: 0,
+                ease: "power2.inOut",
+            });
         });
     });
 
