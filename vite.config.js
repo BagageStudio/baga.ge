df8b4bd42af682e7ff2d6688841f278fe58a984e
Clément Lemoine <clementlemoine.cl@gmail.com>
2023-03-08 17:11:56 +0100

    🚀 preloading img and fonts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 // vite.config.js
 import glsl from "vite-plugin-glsl";
+import VitePluginInjectPreload from "vite-plugin-inject-preload";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [glsl()],
+    plugins: [
+        glsl(),
+        VitePluginInjectPreload({
+            files: [
+                {
+                    match: /[a-z-0-9]*.(png|jpg|jpeg|woff2)$/,
+                },
+            ],
+        }),
+    ],
 });
