f49f0b1196bb99fb6d1246b0c950c9a1be60451e
Clément Lemoine <clementlemoine.cl@gmail.com>
2023-09-08 13:45:51 +0200

    🐛 metas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,39 @@ import glsl from "vite-plugin-glsl";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    app: {
+        head: {
+            meta: [
+                {
+                    charset: "utf-8",
+                },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1.0",
+                },
+            ],
+            link: [
+                {
+                    rel: "icon",
+                    href: "/favicon.ico",
+                    sizes: "any",
+                },
+                {
+                    rel: "icon",
+                    href: "/icon.svg",
+                    type: "image/svg+xml",
+                },
+                {
+                    rel: "apple-touch-icon",
+                    href: "/apple-touch-icon.png",
+                },
+                {
+                    rel: "manifest",
+                    href: "/manifest.webmanifest",
+                },
+            ],
+        },
+    },
     css: ["@/assets/scss/main.scss"],
     build: {
         transpile: ["gsap", "ogl"],
