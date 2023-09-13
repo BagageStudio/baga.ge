20fe87713993339d337dd32d74e4ec502f031f5e
Clément Lemoine <clementlemoine.cl@gmail.com>
2023-09-13 15:55:13 +0200

    grid

--- app.vue
+++ app.vue
@@ -2,12 +2,38 @@
     <div id="app">
         <div class="wrapper-animation" id="c"></div>
         <NuxtPage />
+        <div class="container" v-if="showGrid">
+            <div class="grid" id="grid">
+                <div class="container">
+                    <div class="column-block">
+                        <div class="column-text"></div>
+                    </div>
+                    <div class="column-block">
+                        <div class="column-text"></div>
+                    </div>
+                    <div class="column-block">
+                        <div class="column-text"></div>
+                    </div>
+                    <div class="column-block">
+                        <div class="column-text"></div>
+                    </div>
+                    <div class="column-block">
+                        <div class="column-text"></div>
+                    </div>
+                    <div class="column-block">
+                        <div class="column-text"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
 import WebGL from "~/assets/js/WebGL";
 import { CreateScroll } from "~/assets/js/scroll/home";
+
+const showGrid = ref(false);
 
 const nuxtApp = useNuxtApp();
 
