55dbf700e5e8b86ff35a4335fe918b781e69b64b
menthe <adrien.le.mentheour@gmail.com>
2023-09-29 09:43:04 +0200

    🔍 html lang attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
     app: {
         head: {
             title: "Bagage — Creative development studio",
+            htmlAttrs: {
+                lang: "en",
+            },
             meta: [
                 {
                     charset: "utf-8",
