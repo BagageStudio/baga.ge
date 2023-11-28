3c04f0b8b431e02ff154f5dd8292c6c4e92ab698
menthe <adrien.le.mentheour@gmail.com>
2023-11-28 18:06:14 +0100

    ✨ testimonial page

---
+++ pages/testimonial.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div data-tf-live="01HGB6MAF5Z406E72ETQ0XCXJ8"></div>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    meta: [
+        {
+            property: "og:image",
+            content: "https://baga.ge/bagage.jpg",
+        },
+        {
+            property: "og:image:secure_url",
+            content: "https://baga.ge/bagage.jpg",
+        },
+    ],
+    link: [
+        {
+            rel: "icon",
+            href: "/icon.svg",
+            type: "image/svg+xml",
+        },
+        {
+            rel: "apple-touch-icon",
+            href: "/apple-touch-icon.png",
+        },
+        {
+            rel: "manifest",
+            href: "/manifest.webmanifest",
+        },
+        {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#f24f2b",
+        },
+    ],
+    script: [
+        {
+            src: "//embed.typeform.com/next/embed.js",
+        },
+    ],
+});
+</script>
+
+<style lang="scss" scoped></style>
