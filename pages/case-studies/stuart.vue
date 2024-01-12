850d6ff88a05f2f2e13065828c84c6f31bf01e5d
menthe <adrien.le.mentheour@gmail.com>
2024-01-12 12:05:17 +0100

    ✨ case study credits

--- pages/case-studies/stuart.vue
+++ pages/case-studies/stuart.vue
@@ -7,29 +7,105 @@
     >
         <Header />
         <div class="case-study container">
-            <div class="mod wrapper-fifty-fifty reversed">
-                <div class="col-fifty-fifty">
-                    <h2>
-                        Driving Seamless CRM Integration and Marketing Strategy
-                        for Stuart's Growth
-                    </h2>
-                    <p>
-                        Since the initial development of the website, we've been
-                        supporting Stuart's marketing teams in the
-                        implementation of their acquisition strategy.
-                    </p>
-                    <p>
-                        This includes, of course, connecting to the APIs of the
-                        various CRMs used by their teams. In this context, we
-                        have migrated from Salesforce to HubSpot, and in the CMS
-                        we enable the creation of forms that are linked to
-                        campaigns, while maintaining a consistent graphic style.
-                        At the same time, we've implemented GDPR compliance and
-                        analytics, using Piwik PRO.
-                    </p>
+            <div class="case-study-content">
+                <div class="mod wrapper-fifty-fifty reversed">
+                    <div class="col-fifty-fifty">
+                        <h2>
+                            Driving Seamless CRM Integration and Marketing
+                            Strategy for Stuart's Growth
+                        </h2>
+                        <p>
+                            Since the initial development of the website, we've
+                            been supporting Stuart's marketing teams in the
+                            implementation of their acquisition strategy.
+                        </p>
+                        <p>
+                            This includes, of course, connecting to the APIs of
+                            the various CRMs used by their teams. In this
+                            context, we have migrated from Salesforce to
+                            HubSpot, and in the CMS we enable the creation of
+                            forms that are linked to campaigns, while
+                            maintaining a consistent graphic style. At the same
+                            time, we've implemented GDPR compliance and
+                            analytics, using Piwik PRO.
+                        </p>
+                    </div>
+                    <div class="col-fifty-fifty col-img square">
+                        <div class="dithered-bg">
+                            <img
+                                width="1273"
+                                height="716"
+                                src="~/assets/img/case-studies/stuart/stuart-screen.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div class="col-fifty-fifty col-img square">
-                    <div class="dithered-bg">
+                <div class="mod wrapper-img-full">
+                    <img
+                        width="1273"
+                        height="716"
+                        src="~/assets/img/case-studies/stuart/stuart-screen.png"
+                        alt=""
+                    />
+                </div>
+                <div class="mod wrapper-fifty-fifty">
+                    <div class="col-fifty-fifty col-title">
+                        <h2>
+                            Stuart's Courier Space Redefined with Nuxt 3 and
+                            Dato CMS
+                        </h2>
+                    </div>
+                    <div class="col-fifty-fifty">
+                        <p>
+                            In late 2022, early 2023, we worked on the
+                            migration/reconstruction of the courier space on an
+                            associated sub-domain. The complexity of this
+                            exercise lay mainly in the fact that we had to
+                            create a new site on a Nuxt 3 (stuart.com is on Nuxt
+                            2) and on a new Dato project, while reusing modules
+                            present on the B2B site.<br />
+                            The registration forms for the courier platform are
+                            connected to the Fountain API.
+                        </p>
+                        <p>
+                            We regularly audit both sites with the Seobility
+                            tool, which gives us an overall SEO score and
+                            solutions for improvement.
+                        </p>
+                    </div>
+                </div>
+                <div class="mod wrapper-img-full">
+                    <img
+                        width="1273"
+                        height="716"
+                        src="~/assets/img/case-studies/stuart/stuart-screen.png"
+                        alt=""
+                    />
+                </div>
+                <div class="mod wrapper-fifty-fifty">
+                    <div class="col-fifty-fifty">
+                        <h2>
+                            Driving Seamless CRM Integration and Marketing
+                            Strategy for Stuart's Growth
+                        </h2>
+                        <p>
+                            Since the initial development of the website, we've
+                            been supporting Stuart's marketing teams in the
+                            implementation of their acquisition strategy.
+                        </p>
+                        <p>
+                            This includes, of course, connecting to the APIs of
+                            the various CRMs used by their teams. In this
+                            context, we have migrated from Salesforce to
+                            HubSpot, and in the CMS we enable the creation of
+                            forms that are linked to campaigns, while
+                            maintaining a consistent graphic style. At the same
+                            time, we've implemented GDPR compliance and
+                            analytics, using Piwik PRO.
+                        </p>
+                    </div>
+                    <div class="col-fifty-fifty col-img square">
                         <img
                             width="1273"
                             height="716"
@@ -38,77 +114,28 @@
                         />
                     </div>
                 </div>
-            </div>
-            <div class="mod wrapper-img-full">
-                <img
-                    width="1273"
-                    height="716"
-                    src="~/assets/img/case-studies/stuart/stuart-screen.png"
-                    alt=""
-                />
-            </div>
-            <div class="mod wrapper-fifty-fifty">
-                <div class="col-fifty-fifty col-title">
-                    <h2>
-                        Stuart's Courier Space Redefined with Nuxt 3 and Dato
-                        CMS
-                    </h2>
-                </div>
-                <div class="col-fifty-fifty">
-                    <p>
-                        In late 2022, early 2023, we worked on the
-                        migration/reconstruction of the courier space on an
-                        associated sub-domain. The complexity of this exercise
-                        lay mainly in the fact that we had to create a new site
-                        on a Nuxt 3 (stuart.com is on Nuxt 2) and on a new Dato
-                        project, while reusing modules present on the B2B
-                        site.<br />
-                        The registration forms for the courier platform are
-                        connected to the Fountain API.
-                    </p>
-                    <p>
-                        We regularly audit both sites with the Seobility tool,
-                        which gives us an overall SEO score and solutions for
-                        improvement.
-                    </p>
-                </div>
-            </div>
-            <div class="mod wrapper-img-full">
-                <img
-                    width="1273"
-                    height="716"
-                    src="~/assets/img/case-studies/stuart/stuart-screen.png"
-                    alt=""
-                />
-            </div>
-            <div class="mod wrapper-fifty-fifty">
-                <div class="col-fifty-fifty">
-                    <h2>
-                        Driving Seamless CRM Integration and Marketing Strategy
-                        for Stuart's Growth
-                    </h2>
-                    <p>
-                        Since the initial development of the website, we've been
-                        supporting Stuart's marketing teams in the
-                        implementation of their acquisition strategy.
-                    </p>
-                    <p>
-                        This includes, of course, connecting to the APIs of the
-                        various CRMs used by their teams. In this context, we
-                        have migrated from Salesforce to HubSpot, and in the CMS
-                        we enable the creation of forms that are linked to
-                        campaigns, while maintaining a consistent graphic style.
-                        At the same time, we've implemented GDPR compliance and
-                        analytics, using Piwik PRO.
-                    </p>
-                </div>
-                <div class="col-fifty-fifty col-img square">
-                    <img
-                        width="1273"
-                        height="716"
-                        src="~/assets/img/case-studies/stuart/stuart-screen.png"
-                        alt=""
-                    />
+                <div class="credits">
+                    <div class="col-title content-pad">
+                        <h2>Credits</h2>
+                    </div>
+                    <div class="col-credits credits-details">
+                        <div class="credits-detail content-pad">
+                            <div class="detail-title">Design</div>
+                            <span
+                                >Stuart in-house team (Pablo Vene, Clara
+                                Quinteros, Kathya Gonzales)</span
+                            >
+                        </div>
+                        <div class="credits-detail content-pad">
+                            <div class="detail-title">Development</div>
+                            <span>Viktoriia Zakorchemna</span>
+                            <span>Bagage</span>
+                        </div>
+                        <div class="credits-detail content-pad">
+                            <div class="detail-title">Font</div>
+                            <span>Grilli Type</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="contact">
@@ -220,7 +247,69 @@ definePageMeta({
     padding-top: 120px;
     background-color: $black;
 }
+.case-study-content {
+    padding: 0 var(--gutter-out-monolith);
+}
 footer {
     background-color: $black;
+}
+.col-credits {
+    position: relative;
+    padding-bottom: 20px;
+    &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: var(--gutter);
+        right: var(--gutter);
+        border-bottom: 1px solid var(--color);
+    }
+}
+.credits-detail {
+    &:not(:last-child) {
+        margin-bottom: 20px;
+    }
+    > span {
+        display: block;
+    }
+}
+.detail-title {
+    margin-bottom: 8px;
+    font-feature-settings: "ss16";
+    text-transform: uppercase;
+    font-size: 2.1rem;
+}
+
+@media (min-width: $phone) {
+    .col-credits {
+        display: flex;
+        flex-wrap: wrap;
+        padding-bottom: 40px;
+    }
+    .credits-detail {
+        flex: 0 0 auto;
+        width: 50%;
+        &:not(:last-child) {
+            margin-bottom: 0;
+        }
+        margin-bottom: 0;
+        &:nth-last-child(n + 3) {
+            margin-bottom: 35px;
+        }
+        > span {
+            display: block;
+        }
+    }
+}
+
+@media (min-width: $desktop) {
+    .credits {
+        display: flex;
+        align-items: flex-start;
+    }
+    .col-title {
+        flex: 0 0 auto;
+        width: percentage(math.div(2, 6));
+    }
 }
 </style>
